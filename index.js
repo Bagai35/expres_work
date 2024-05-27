f694f50b18ba0593412ea83085d02bd3a662fa8f
Bogdan Keplast <bogdan.keplast@gmail.com>
2024-05-27 14:14:43 +0300

    z

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+app.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname, 'views', 'index.html'))
+})
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(_dirname, 'views', 'about.html'))
+})
+
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
 
 })
-
