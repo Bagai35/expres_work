4813882d07fe118470e71072b854a19676121d16
Bagai35 <bogdan.keplast@gmail.com>
2024-05-28 10:45:21 +0300

    update

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'view/index.html'))
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'view/about.html'))
-})
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
-
-})
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', courseesRoutes)
